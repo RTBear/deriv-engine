ca98ec44e3fe3b9db8d521e9f8511608b94c1db5
Ryan Mecham <ryan.mecham@aggiemail.usu.edu>
2019-03-30 13:14:20 -0600

    solve hw9 p2

--- hw09_s19.py
+++ hw09_s19.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 import csv
 import graph
+import scipy.integrate
 
 
 def generate_file_names(ftype, rootdir):
@@ -54,6 +55,33 @@ def plot_bee_traffic(csv_fp):
     data = [up,down,lateral]
     graph.graphFromTable(xvals,data,labels,name='Bee traffic for '+csv_fp)
 
+def sr_approx(f, a, b, n):#alias for simpson_approx
+    return simpson_approx(f, a, b, n)
+
+def sp_approx(f, a, b, n):#alias for simpson_approx
+    return simpson_approx(f, a, b, n)
+
+def simpson_approx(f, a, b, n):
+    dx = (b - a) / float(n)
+    # print('dx',dx)
+    subdivisions = np.linspace(a,b,n+1)
+    # print('--dx',subdivisions[1] - subdivisions[0])
+    # print(subdivisions, len(subdivisions))
+
+    inner_sum = 0
+
+    for i,x in enumerate(subdivisions):
+        if i == 0 or i == n:#start or end
+            mult = 1
+        else:
+            if i%2 == 0:#even x
+                mult = 2
+            else:
+                mult = 4
+        inner_sum += mult*f(x)
+    approx = (dx/3.0) * inner_sum
+    return approx
+
 def bee_traffic_estimate(t, md='u', fd={}):
     assert md == 'u' or md == 'd' or md == 'l'
     vals = fd.get(int(t))
@@ -70,10 +98,11 @@ def make_bee_traffic_estimator(fd, md):
     assert md == 'u' or md == 'd' or md == 'l'
     return lambda t: bee_traffic_estimate(t, md=md, fd=fd)
 
-
 def bee_traffic_stats(fd):
-    ## your code here
-    pass
+    up_bte = make_bee_traffic_estimator(fd, 'u')
+    down_bte = make_bee_traffic_estimator(fd, 'd')
+    lat_bte = make_bee_traffic_estimator(fd, 'l')
+    return(sr_approx(up_bte, 5, 28, 23),sr_approx(down_bte, 5, 28, 23),sr_approx(lat_bte, 5, 28, 23))
 
 def find_smallest_up_down_gap_file(csv_dir):
     ## your code here
@@ -113,6 +142,42 @@ def find_min_lat_file(csv_dir):
     ## your code here
     pass
 
+
+def testFunc(csv_fp):
+    FD = read_csv_file(csv_fp)
+    up_bte = make_bee_traffic_estimator(FD, 'u')
+    down_bte = make_bee_traffic_estimator(FD, 'd')
+    lat_bte = make_bee_traffic_estimator(FD, 'l')
+    print(sr_approx(up_bte, 5, 28, 23))
+    print(sr_approx(down_bte, 5, 28, 23))
+    print(sr_approx(lat_bte, 5, 28, 23))
+
 if __name__ == '__main__':
-    plot_bee_traffic('./bee_traffic_estimates/192_168_4_5-2018-07-01_08-00-10.csv')
-  
+    csv_fp = './bee_traffic_estimates/192_168_4_5-2018-07-01_08-00-10.csv'
+    # plot_bee_traffic(csv_fp)
+    test = sr_approx(lambda x: x**2, 0, 2, 10)
+    print(test)
+    test = sr_approx(lambda x: x**3, 1, 5, 100)
+    print(test)
+
+    # x = np.linspace(1, 5, 100) #verify simpson approximation
+    # y = np.array([i**3 for i in x])
+    # print(scipy.integrate.simps(y, x))
+    print('-------------')
+    testFunc('./bee_traffic_estimates/192_168_4_5-2018-07-01_08-00-10.csv')
+    print('-------------')
+    testFunc('./bee_traffic_estimates/192_168_4_5-2018-07-01_08-30-10.csv')
+    print('-------------')
+    testFunc('./bee_traffic_estimates/192_168_4_5-2018-07-01_16-30-10.csv')
+    print('-------------')
+
+    fd = read_csv_file('./bee_traffic_estimates/192_168_4_5-2018-07-02_16-30-10.csv')
+    print(bee_traffic_stats(fd))
+    print('-------------')
+
+    fd = read_csv_file('./bee_traffic_estimates/192_168_4_5-2018-07-01_14-00-10.csv')
+    print(bee_traffic_stats(fd))
+    print('-------------')
+
+    fd = read_csv_file('./bee_traffic_estimates/192_168_4_5-2018-07-01_18-00-10.csv')
+    print(bee_traffic_stats(fd))
